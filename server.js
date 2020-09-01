2bee6dfa74766c40ff5fc749fc38f72d62dfaf30
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-09-01 23:13:02 +0200

    task-27.3

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const hbs = require('express-handlebars');
 
-app.use((req, res, next) => {
-    res.show = (name) => {
-      res.sendFile(path.join(__dirname, `/views/${name}`));
-    };
-    next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/user', (req, res, next) => {
-    res.show('user.html');
+    res.render('user');
 });
 
-app.get('/home', (req, res) => {
-    res.show('index.html');
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about', { layout: 'dark'});
 });
 
 app.use((req, res) => {
-    res.status(404).show('404.html');
+    res.status(404).send('404 not found...');
 })
 
 app.listen(8000, () => {
