23d337d36eb1ad575786a82143b44f91ced2ec29
Bartosz Glinka <bartosz.glinka@hotmail.com--global>
2020-09-03 21:19:02 +0200

    taskt 27.3, 27.4

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const path = require('path');
+const multer  = require('multer')
+const upload = multer()
+
 const app = express();
 const hbs = require('express-handlebars');
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')));
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.post('/contact/send-message', upload.single('project'), (req, res) => {
+
+    const { author, sender, title, message } = req.body;
+
+    if(author && sender && title && req.file && message ) {
+        res.render('contact', { isSent: true, name: req.file.fieldname });
+      }
+    else {
+        res.render('contact', { isError: true });
+    }
+  });
+
+app.use('/contact', (req, res, next) => {
+    res.render('contact');
+});
 
 app.use('/user', (req, res, next) => {
     res.render('user');
@@ -24,7 +43,7 @@ app.get('/about', (req, res) => {
 app.use((req, res) => {
     res.status(404).send('404 not found...');
 })
-
+  
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
